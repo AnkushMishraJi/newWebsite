57c657f164cf5b0b4cf7f3512812f61bdae934f4
AnkushMishraJi <ankush.rdso@gmail.com>
2021-12-23 09:24:44 +0530

    screen 1 design

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,68 +1,90 @@
 import React, { useEffect, useContext } from "react";
 import { Link, useHistory } from "react-router-dom";
 import "../App.css";
-import * as ReactBootStrap from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHome,
+  faUser,
+  faConciergeBell,
+  faBookmark,
+} from "@fortawesome/free-solid-svg-icons";
+
 function NavigationBar() {
   const login = localStorage.getItem("businessLoggedIn");
   return (
-    <ReactBootStrap.Navbar
-      collapseOnSelect
-      expand="xl"
-      bg="primary"
-      variant="dark"
+    // <ReactBootStrap.Navbar
+    //   collapseOnSelect
+    //   expand="xl"
+    //   bg="primary"
+    //   variant="dark"
+    // >
+    //   {login !== "true" ? (
+    //     <ReactBootStrap.Navbar.Brand
+    //       style={{
+    //         width: "55%",
+    //         marginLeft: "10px",
+    //       }}
+    //       href="/"
+    //     >
+    //       MERA ADDA
+    //     </ReactBootStrap.Navbar.Brand>
+    //   ) : (
+    //     <ReactBootStrap.Navbar.Brand
+    //       style={{ width: "55%", marginLeft: "10px" }}
+    //       href="/hoteladmin"
+    //     >
+    //       MERA ADDA
+    //     </ReactBootStrap.Navbar.Brand>
+    //   )}
+    //   <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    //   <ReactBootStrap.Navbar.Collapse
+    //     style={{
+    //       backgroundColor: "blue",
+    //       height: "50%",
+    //       zIndex: "1020",
+    //       justifyContent: "space-around",
+    //     }}
+    //     id="responsive-navbar-nav"
+    //   >
+    //     <ReactBootStrap.Nav>
+    //       <Link style={{ textDecoration: "none" }} to="/bsignup">
+    //         <ReactBootStrap.Nav.Link href="/bsignup">
+    //           Business Sign-up
+    //         </ReactBootStrap.Nav.Link>
+    //       </Link>
+    //       <Link style={{ textDecoration: "none" }} to="/bsignin">
+    //         <ReactBootStrap.Nav.Link href="/bsignin">
+    //           Business Sign-in
+    //         </ReactBootStrap.Nav.Link>
+    //       </Link>
+    //       <Link style={{ textDecoration: "none" }} to="/uphone">
+    //         <ReactBootStrap.Nav.Link href="/uphone">
+    //           User Sign-in
+    //         </ReactBootStrap.Nav.Link>
+    //       </Link>
+    //       <Link style={{ textDecoration: "none" }} to="/usignup">
+    //         <ReactBootStrap.Nav.Link href="/usignup">
+    //           User Sign-up
+    //         </ReactBootStrap.Nav.Link>
+    //       </Link>
+    //     </ReactBootStrap.Nav>
+    //   </ReactBootStrap.Navbar.Collapse>
+    // </ReactBootStrap.Navbar>
+    <div
+      className="d-flex w-100 align-items-center"
+      style={{
+        position: "fixed",
+        bottom: "0",
+        backgroundColor: "white",
+        height: "5%",
+        fontSize: "18px",
+      }}
     >
-      {login !== "true" ? (
-        <ReactBootStrap.Navbar.Brand
-          style={{
-            width: "55%",
-            marginLeft: "10px",
-          }}
-          href="/"
-        >
-          MERA ADDA
-        </ReactBootStrap.Navbar.Brand>
-      ) : (
-        <ReactBootStrap.Navbar.Brand
-          style={{ width: "55%", marginLeft: "10px" }}
-          href="/hoteladmin"
-        >
-          MERA ADDA
-        </ReactBootStrap.Navbar.Brand>
-      )}
-      <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <ReactBootStrap.Navbar.Collapse
-        style={{
-          backgroundColor: "blue",
-          height: "50%",
-          zIndex: "1020",
-          justifyContent: "space-around",
-        }}
-        id="responsive-navbar-nav"
-      >
-        <ReactBootStrap.Nav>
-          <Link style={{ textDecoration: "none" }} to="/bsignup">
-            <ReactBootStrap.Nav.Link href="/bsignup">
-              Business Sign-up
-            </ReactBootStrap.Nav.Link>
-          </Link>
-          <Link style={{ textDecoration: "none" }} to="/bsignin">
-            <ReactBootStrap.Nav.Link href="/bsignin">
-              Business Sign-in
-            </ReactBootStrap.Nav.Link>
-          </Link>
-          <Link style={{ textDecoration: "none" }} to="/uphone">
-            <ReactBootStrap.Nav.Link href="/uphone">
-              User Sign-in
-            </ReactBootStrap.Nav.Link>
-          </Link>
-          <Link style={{ textDecoration: "none" }} to="/usignup">
-            <ReactBootStrap.Nav.Link href="/usignup">
-              User Sign-up
-            </ReactBootStrap.Nav.Link>
-          </Link>
-        </ReactBootStrap.Nav>
-      </ReactBootStrap.Navbar.Collapse>
-    </ReactBootStrap.Navbar>
+      <FontAwesomeIcon className="mx-auto" icon={faHome} />
+      <FontAwesomeIcon className="mx-auto" icon={faConciergeBell} />
+      <FontAwesomeIcon className="mx-auto" icon={faBookmark} />
+      <FontAwesomeIcon className="mx-auto" icon={faUser} />
+    </div>
   );
 }
 export default NavigationBar;
